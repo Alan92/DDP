744fce380dd9386b8b08c543919a8794c6026991
Alan92 <alan_92_@hotmail.es>
2012-10-29 20:08:09 +0000

    Quinto commit

--- lib/ppt.rb
+++ lib/ppt.rb
@@ -1,2 +1,6 @@
 class Ppt
+attr_accessor :humano
+def initialize
+@humano = 0
+end
 end
--- spec/ppt_spec.rb
+++ spec/ppt_spec.rb
@@ -3,5 +3,11 @@ describe Ppt do
 before :each do
 @ppt_obj=Ppt.new
 end
+
+it "Deberia de haber una tirada del humano" do
+@ppt_obj.humano.should == 0
+end
+
+
 end
 
